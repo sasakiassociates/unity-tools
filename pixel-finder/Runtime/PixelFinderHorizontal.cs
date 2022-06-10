5289a92f5b24807a89828a3dbfe4d81fbd06f8ad
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-06-09 21:53:58 -0400

    system types

---
+++ pixel-finder/Runtime/PixelFinderHorizontal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Sasaki.Unity
+{
+
+	public class PixelFinderHorizontal : PixelFinderSystem
+	{
+		internal override IEnumerable<FinderDirection> finderSetups =>
+			new[]
+			{
+				FinderDirection.Front,
+				FinderDirection.Left,
+				FinderDirection.Back,
+				FinderDirection.Right
+			};
+	}
+}
