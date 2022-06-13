13e082a3591cd7c2c9fe50150cdc1f9306058888
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-06-13 14:54:52 -0400

    cleaning

--- pixel-finder/Runtime/Test/PixelFinderSystemTester.cs
+++ pixel-finder/Runtime/Test/PixelFinderSystemTester.cs
@@ -1,29 +1,16 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Sasaki.Unity
 {
-	public enum SystemType
-	{
-		Coroutines,
-		Task,
-		Update,
-		Sync
-	}
 
 	public class PixelFinderSystemTester : MonoBehaviour
 	{
 		[SerializeField] private int pointCount;
 
-		// [SerializeField] bool useSync;
-		[SerializeField] SystemType systemType;
-		[SerializeField] PixelFinderGPUCallback _gpuCallback;
-
 		[SerializeField] GameObject frontObj, leftObj, rightObj, backObj;
-
-		// [SerializeField] RawImage frontImage, leftImage, rightImage, backImage;
-
-		public PixelFinderSystem system;
+		
+		public APixelFinderSystem system;
 
 		Vector3[] points;
 
@@ -44,45 +31,22 @@ namespace Sasaki.Unity
 		void OnGUI()
 		{
 			if (GUI.Button(new Rect(10, 10, 50, 15), "Run"))
-				Run().Forget();
-
-			if (GUI.Button(new Rect(10, 50, 50, 15), "Render"))
-			{
-				StartCoroutine(_gpuCallback.Run());
-			}
+				Run();
+			
 		}
 
-		void Check()
-		{ }
-		
-		
-		public async UniTaskVoid Run()
+		public void Run()
 		{
-			Debug.Log($"Starting System Test with {systemType}");
 			points = new Vector3[pointCount];
 
 			for (int i = 0; i < pointCount; i++)
 				points[i] = transform.position;
 
-			system.Create(points, colors);
-			_gpuCallback.Init(colors, Check, points.Length);
+			system.Init(points, colors);
 
-			system.OnSystemDataComplete += arg0 => Debug.Log("Reporting back");
-
-			switch (systemType)
-			{
-				case SystemType.Coroutines:
-					StartCoroutine(system.RunCoroutine());
-					break;
-				case SystemType.Task:
-					await system.RunTask();
-					break;
-				case SystemType.Sync:
-					system.RunSync();
-					break;
-				case SystemType.Update:
-					break;
-			}
+			system.onSystemRunComplete += arg0 => Debug.Log("Reporting back");
+			
+			system.Run();
 		}
 
 	}
--- pixel-finder/Runtime/Test/PixelFinderTester.cs
+++ pixel-finder/Runtime/Test/PixelFinderTester.cs
@@ -19,7 +19,6 @@ namespace Sasaki.Unity
 		[SerializeField] RawImage frontImageJob, leftImageJob, rightImageJob, backImageJob;
 
 		List<PixelFinder> pixelFinder;
-		List<PixelFinderJob> pixelFinderJobs;
 
 		float m_UpdateTime = -1;
 
@@ -31,7 +30,6 @@ namespace Sasaki.Unity
 		void Start()
 		{
 			Test_UseSeparateColorsForEachShader();
-			Test_UseBurst();
 		}
 
 		Stopwatch timer;
@@ -44,16 +42,6 @@ namespace Sasaki.Unity
 				foreach (var finder in pixelFinder)
 					StartCoroutine(finder.Run());
 			
-			else if (systemType is FinderSystemType.BurstParallel or FinderSystemType.Burst)
-			{
-				foreach (var finder in pixelFinderJobs)
-				{
-					finder.usePar = systemType == FinderSystemType.BurstParallel;
-					StartCoroutine(finder.Run());
-				}
-			}
-
-
 		}
 
 		void OnGUI()
@@ -78,64 +66,12 @@ namespace Sasaki.Unity
 				foreach (var finder in pixelFinder)
 					Debug.Log(finder.data.Data[0][0]);
 			}
-			else if (systemType is FinderSystemType.BurstParallel or FinderSystemType.Burst)
-			{
-				foreach (var finder in pixelFinderJobs)
-				{
-					if (!finder.isDone)
-					{
-						Debug.Log($"{finder.name} is not done");
-						return;
- 					}
-				}
 
-				foreach (var finder in pixelFinderJobs)
-					Debug.Log(finder.data.Data[0][0]);
-			}
-			
 			timer.Stop();
 
 			Debug.Log($"Complete {systemType}: {timer.Elapsed}");
 		}
-
-		void Test_UseBurst()
-		{
-			var parent = new GameObject("Jobs");
-
-			var front = new GameObject("Front-PixelFinderJob").AddComponent<PixelFinderJob>();
-			front.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
-			front.transform.SetParent(parent.transform);
-
-			front.Init(frontObj.GetComponent<MeshRenderer>().material.GetColor(DiffuseColor), Check);
-			frontImageJob.texture = front.texture;
-
-			var left = new GameObject("Left-PixelFinderJob").AddComponent<PixelFinderJob>();
-			left.transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
-			left.transform.SetParent(parent.transform);
-
-			left.Init(leftObj.GetComponent<MeshRenderer>().material.GetColor(DiffuseColor), Check);
-			leftImageJob.texture = left.texture;
-
-			var right = new GameObject("Right-PixelFinderJob").AddComponent<PixelFinderJob>();
-			right.transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
-			right.transform.SetParent(parent.transform);
-
-			right.Init(rightObj.GetComponent<MeshRenderer>().material.GetColor(DiffuseColor), Check);
-			rightImageJob.texture = right.texture;
-
-			var back = new GameObject("Back-PixelFinderJob").AddComponent<PixelFinderJob>();
-			back.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
-			back.transform.SetParent(parent.transform);
-
-			back.Init(backObj.GetComponent<MeshRenderer>().material.GetColor(DiffuseColor), Check);
-			backImageJob.texture = back.texture;
-
-			pixelFinderJobs = new List<PixelFinderJob>()
-			{
-				front, left, right, back
-			};
-		}
-
+		
 		void Test_UseSeparateColorsForEachShader()
 		{
 			var parent = new GameObject("Shader");
