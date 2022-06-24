97be9cba0c2809d8b682f86a498987867af61c73
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-06-24 17:34:02 -0400

    fix: blocking files with ignore

---
+++ .releaserc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ["main"],
+  preset: "angular",
+  extends: "semantic-release-monorepo",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message: "chore(release): ${nextRelease.gitTag} [skip ci]",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
