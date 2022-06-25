190800e2100bbc3242dea183c53e93422d2e5333
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-06-24 20:03:34 -0400

    added changelog presets

--- .releaserc.js
+++ .releaserc.js
@@ -4,30 +4,36 @@ module.exports = {
   extends: "semantic-release-monorepo",
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    {
-      parserOpts: {
-        noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+        },
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            { type: "build", section: "Build System", hidden: false },
+            { type: "chore", section: "Build System", hidden: false },
+            { type: "ci", section: "Continuous Integration", hidden: false },
+            { type: "docs", section: "Documentation", hidden: false },
+            { type: "feat", section: "Features", hidden: false },
+            { type: "fix", section: "Bug Fixes", hidden: false },
+            {
+              type: "perf",
+              section: "Performance Improvements",
+              hidden: false,
+            },
+            { type: "refactor", section: "Code Refactoring", hidden: false },
+            { type: "style", section: "Styles", hidden: false },
+            { type: "test", section: "Tests", hidden: false },
+          ],
+        },
+        writerOpts: {
+          commitsSort: ["subject", "scope"],
+        },
       },
-      preset: "conventionalcommits",
-      presetConfig: {
-        types: [
-          { type: "build", section: "Build System", hidden: false },
-          { type: "chore", section: "Build System", hidden: false },
-          { type: "ci", section: "Continuous Integration", hidden: false },
-          { type: "docs", section: "Documentation", hidden: false },
-          { type: "feat", section: "Features", hidden: false },
-          { type: "fix", section: "Bug Fixes", hidden: false },
-          { type: "perf", section: "Performance Improvements", hidden: false },
-          { type: "refactor", section: "Code Refactoring", hidden: false },
-          { type: "style", section: "Styles", hidden: false },
-          { type: "test", section: "Tests", hidden: false },
-        ],
-      },
-      writerOpts: {
-        commitsSort: ["subject", "scope"],
-      },
-    },
+    ],
     "@semantic-release/changelog",
     "@semantic-release/npm",
     [
