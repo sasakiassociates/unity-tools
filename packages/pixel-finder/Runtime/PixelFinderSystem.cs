37884ee13bfdf1e5292ae929f45c47c24da9dc71
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-07-14 15:58:14 -0400

    fix(finder): Improved system calls
    removed virtual access to certain function calls and created hooks for pre and post move logic

--- packages/pixel-finder/Runtime/PixelFinderSystem.cs
+++ packages/pixel-finder/Runtime/PixelFinderSystem.cs
@@ -5,57 +5,50 @@ using UnityEngine.Events;
 
 namespace Sasaki.Unity
 {
+	/// <summary>
+	///   A system object that handles positioning a series of layouts throughout a set of points
+	/// </summary>
+	[AddComponentMenu("Sasaki/PixelFinder/FinderSystem")]
 	public class PixelFinderSystem : MonoBehaviour
 	{
 		[SerializeField] List<PixelFinderLayout> _layouts;
 
-		[SerializeField, HideInInspector] Vector3[] _points;
+		[SerializeField] [HideInInspector] Vector3[] _points;
 
-		[SerializeField, HideInInspector] int _index;
+		[SerializeField] [HideInInspector] int _index;
 
 		(bool auto, bool running, bool compelte) _is;
 
+		/// <summary>
+		///   Option for having the system move to the next point once the layouts are complete.
+		/// </summary>
 		public bool autoRun
 		{
 			get => _is.auto;
 			set => _is.auto = value;
 		}
 
+		/// <summary>
+		///   Returns true if layouts are working or the system is not through each point
+		/// </summary>
 		public bool isRunning
 		{
 			get => _is.running;
 			protected set => _is.running = value;
 		}
 
+		/// <summary>
+		///   Returns true if all layouts are done running and each point has been hit
+		/// </summary>
 		public bool isComplete
 		{
 			get => _is.compelte;
 			protected set => _is.compelte = value;
 		}
 
-		public List<PixelFinderLayout> layouts
-		{
-			get => _layouts;
-			protected set => _layouts = value;
-		}
-
-		public Vector3[] points
-		{
-			get => _points;
-			protected set => _points = value;
-		}
-
-		public int pointIndex
-		{
-			get => _index;
-			protected set => _index = value;
-		}
-
-		protected virtual IFinderSystemData dataContainer
-		{
-			get => new FinderSystemDataContainer(_layouts, name);
-		}
-
+		/// <summary>
+		///   Returns true if all layouts have completed their tasks
+		/// </summary>
 		public bool allDone
 		{
 			get
@@ -68,33 +61,104 @@ namespace Sasaki.Unity
 			}
 		}
 
+		/// <summary>
+		///   Layouts used within this system
+		/// </summary>
+		public List<PixelFinderLayout> layouts
+		{
+			get => _layouts;
+			protected set => _layouts = value;
+		}
+
+		/// <summary>
+		///   Stored points for the system to move to
+		/// </summary>
+		public Vector3[] points
+		{
+			get => _points;
+			protected set => _points = value;
+		}
+
+		/// <summary>
+		///   Current point index for the finder
+		/// </summary>
+		public int pointIndex
+		{
+			get => _index;
+			protected set => _index = value;
+		}
+
+		/// <summary>
+		///   Returns the count of points
+		/// </summary>
+		public int collectionSize
+		{
+			get => points?.Length ?? 0;
+		}
+
+		/// <summary>
+		///   Copies all the data in this system
+		/// </summary>
+		protected virtual IFinderSystemData dataContainer
+		{
+			get => new FinderSystemDataContainer(_layouts, name);
+		}
+
+		/// <summary>
+		///   Sets the culling mask to all viewers within the system
+		/// </summary>
+		public int mask
+		{
+			set
+			{
+				foreach (var layout in layouts)
+					layout.mask = value;
+			}
+		}
+
+		/// <summary>
+		///   Clears all active layouts in the system
+		/// </summary>
 		public void ClearLayouts()
 		{
 			if (_layouts != null && _layouts.Any())
-				for (int i = _layouts.Count - 1; i >= 0; i--)
+				for (var i = _layouts.Count - 1; i >= 0; i--)
 					Destroy(_layouts[i].gameObject);
 		}
 
-		protected void ResetDataContainer(int collectionSize)
+		/// <summary>
+		///   Clears all values and creates a new collection for the size of the current point count
+		/// </summary>
+		protected void ResetDataContainer()
 		{
+			Debug.Log($"{name} is resetting data");
+
 			if (layouts != null && layouts.Any())
 				foreach (var f in layouts)
 					f.ResetDataContainer(collectionSize);
 		}
 
+		/// <summary>
+		///   Initializes a new system with layouts. Stores the points internally and passes the colors into the finders
+		/// </summary>
+		/// <param name="systemPoints"></param>
+		/// <param name="color"></param>
+		/// <param name="inputLayouts"></param>
 		public void Init(Vector3[] systemPoints, Color32 color, List<PixelFinderLayout> inputLayouts = null)
 		{
 			Init(systemPoints, new[] { color }, inputLayouts);
 		}
 
 		/// <summary>
-		/// Initialize the system. Will clear any existing layouts and data
+		///   Initialize the system. Will clear any existing layouts and data
 		/// </summary>
 		/// <param name="systemPoints">Points where analysis will be captured</param>
 		/// <param name="colors">Pixel color that will be searched for</param>
 		/// <param name="inputLayouts">Type of layouts to use with this system</param>
 		public virtual void Init(Vector3[] systemPoints, Color32[] colors, List<PixelFinderLayout> inputLayouts = null)
 		{
+			name = "FinderSystem";
+
 			if (inputLayouts != null && inputLayouts.Any())
 			{
 				ClearLayouts();
@@ -109,10 +173,12 @@ namespace Sasaki.Unity
 				layout.onComplete += CheckLayoutsInSystem;
 				layout.transform.SetParent(transform);
 			}
+
+			Application.targetFrameRate = 200;
 		}
 
 		/// <summary>
-		/// Starts the system
+		///   Starts the system
 		/// </summary>
 		/// <param name="startingIndex">the index to start the system at</param>
 		public void Run(int startingIndex = 0)
@@ -120,30 +186,34 @@ namespace Sasaki.Unity
 			pointIndex = startingIndex;
 
 			isRunning = true;
-			// isComplete = false;
 
 			MoveAndRender();
 		}
 
+		/// <summary>
+		///   Main system call to set a new position and trigger a new render for each finder
+		///   Handles Pre and Post move render calls
+		/// </summary>
 		void MoveAndRender()
 		{
+			PreMoveRender();
+
 			// step move forward
 			transform.position = points[pointIndex];
-			
+
 			foreach (var layout in _layouts)
 				layout.Run(pointIndex);
+
+			PostMoveRender();
 		}
 
+		/// <summary>
+		///   Checks if all layouts are complete and if there are more points to gather from
+		/// </summary>
 		void CheckLayoutsInSystem()
 		{
 			if (allDone)
 			{
-				// if (isComplete)
-				// {
-				// 	Debug.Log($"{name} is already completed");
-				// 	return;
-				// }
-
 				// if we move manually we send the data back at each point
 				if (!autoRun)
 				{
@@ -161,20 +231,32 @@ namespace Sasaki.Unity
 					// isComplete = true;
 				}
 				else
+				{
 					MoveAndRender();
+				}
 			}
 		}
 
+		/// <summary>
+		///   Method to implement any logic after the system has moved
+		/// </summary>
+		protected virtual void PostMoveRender()
+		{ }
+
+		/// <summary>
+		///   Method to implement any logic before the system has moved
+		/// </summary>
+		protected virtual void PreMoveRender()
+		{ }
+
+		/// <summary>
+		///   Calls on complete and compiles the data into a container
+		/// </summary>
 		protected virtual void GatherSystemData()
 		{
-			onComplete?.Invoke(dataContainer);
+			OnComplete?.Invoke(dataContainer);
 		}
 
-		#region Events
-
-		public event UnityAction<IFinderSystemData> onComplete;
-
-		#endregion
-
+		public event UnityAction<IFinderSystemData> OnComplete;
 	}
 }
